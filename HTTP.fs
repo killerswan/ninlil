2b3c2b66b5190a1e320885ab65da71a030fbb4e8
Kevin Cantu <me@kevincantu.org>
2011-04-10 23:54:22 -0700

    hah, remember to add new files...

---
+++ HTTP.fs
@@ -0,0 +1,77 @@
+// Copyright (c) 2011, Kevin Cantu <me@kevincantu.org>
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// The software is provided "as is" and the author disclaims all warranties
+// with regard to this software including all implied warranties of
+// merchantability and fitness.  In no event shall the author be liable for
+// any special, direct, indirect, or consequential damanges or any damages
+// whatsoever resulting from loss of use, data, your immortal soul, or 
+// profits, whether in an action of contract, negligence, or other 
+// tortious action, arising out of or in connection with the use or 
+// performance of this software.
+
+module Ninlil.HTTP
+
+open System.Collections.Generic
+open System.Net
+open System.IO
+open System.Threading
+
+
+// combines key/values into a string with = and &
+// for use with HTTP GET and POST
+let combine (m: Map<string,string>) : string = 
+   Map.fold (fun state key v -> 
+                  let next = key + "=" + v
+                  match state with
+                  | "" ->               next
+                  | _  -> state + "&" + next) 
+            ""
+            m
+
+
+// HTTP GET
+// Note: the point of the async {} is to try not to block so much,
+// but in this program it is over-engineering. :D
+let httpget (url: string) (data: Map<string,string> ) : string = 
+   Async.RunSynchronously(async {
+      let url' = url + "?" + (combine data)
+
+      let req        = WebRequest.Create(url', Timeout=15000)
+      use! response  = req.AsyncGetResponse()
+      use reader     = new StreamReader(response.GetResponseStream())
+      let output = reader.ReadToEnd()
+      return output
+   })
+
+
+// HTTP POST
+let httppost (url: string) (data: Map<string,string>) : string =
+   Async.RunSynchronously(async {
+      let data' : byte[] = System.Text.Encoding.ASCII.GetBytes(combine data);
+
+      let request = WebRequest.Create(url, Timeout=15000)  // sensitive to too short a delay
+      request.Method        <- "POST"
+      request.ContentType   <- "application/x-www-form-urlencoded"
+      request.ContentLength <- (int64) data'.Length
+
+      use wstream = request.GetRequestStream() 
+      wstream.Write(data',0, (data'.Length))
+      wstream.Flush()
+      wstream.Close()
+
+      use! response = request.AsyncGetResponse()
+      use reader    = new StreamReader(response.GetResponseStream())
+      let output    = reader.ReadToEnd()
+
+      reader.Close()
+      response.Close()
+      request.Abort()
+
+      return output
+   })
+
+
